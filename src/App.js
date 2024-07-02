49822ea7962cec931dbe7f8ee3b26295c9070eb1
ChakrabortyDiya <crs.diya@gmail.com>
2024-07-02 12:43:28 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
-import React from 'react'
 import Header from './components/Header'
 
-//function App() {
- // return (
-  //  <div className='container'>
-    //  <Header />
-    //</div>
- // );
-//}
-
-class App extends React.Component {
-  render() {
-    return <h1>hello from a class</h1>
-  }
+function App() {
+ return (
+  <div className='container'>
+    <Header title='Hello'/>
+  </div>
+ );
 }
+
+
 
 
 export default App;
