0579a0b2f397c416e94017a0c5b8eeaa64a2e9ce
ChakrabortyDiya <crs.diya@gmail.com>
2024-07-02 12:57:12 +0530

    check

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 const Header = ({ title }) => {
   return (
     <header>
-      <h1 style={{color: 'red',backgroundColor: 'black'}}>{title}</h1>
+      <h1 style={headingStyle}>{title}</h1>
     </header>
   )
 }
@@ -16,5 +16,11 @@ Header.defaultProps ={
 Header.propTypes={
     title: PropTypes.string.isRequired,
 }
+
+const headingStyle={
+    color: 'red',
+    backgroundColor: 'black',
+}
+
 
 export default Header
