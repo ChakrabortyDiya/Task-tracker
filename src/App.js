1d199a50cb8e4534883323f7375256a46d8c3bbf
ChakrabortyDiya <crs.diya@gmail.com>
2024-07-02 12:30:59 +0530

    trial

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+import React from 'react'
 import Header from './components/Header'
 
-function App() {
-  return (
-    <div className='container'>
-      <Header />
-    </div>
-  );
-}
+//function App() {
+ // return (
+  //  <div className='container'>
+    //  <Header />
+    //</div>
+ // );
+//}
 
 export default App;
