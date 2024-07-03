6a7a2b5f0d5f2ffe5e9d4d20ec1416aa836bb432
ChakrabortyDiya <crs.diya@gmail.com>
2024-07-03 11:09:22 +0530

    changed

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,32 +1,10 @@
-const tasks=[
-    {
-        id:1,
-        text: 'Doctors appointment',
-        day:'Feb 5th at 2:30pm',
-        reminder:true,
-    },
-    {
-        id:2,
-        text: 'Meeting at School',
-        day:'Feb 6th at 1:30pm',
-        reminder:true,    
-    },
-    {
-        id:3,
-        text: 'Grocery Shopping',
-        day:'Feb 5th at 2:30pm',
-        reminder:false,
-    }
-]
-
-const Tasks = () => {
-  return (
+const Tasks =({tasks})=>{
+return (
     <>
-      {tasks.map((tasks)=>(
-      <h3>{tasks.text}</h3>
+      {tasks.map((task)=>(
+      <h3 key={task.id}>{task.text}</h3>
       ))}
     </>
   )
 }
-
 export default Tasks
