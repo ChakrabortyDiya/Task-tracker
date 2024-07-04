e6d180c9ed33209cfe46ee9e8b446b3ab0c2c446
ChakrabortyDiya <crs.diya@gmail.com>
2024-07-04 14:35:20 +0530

    About updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import {BrowserRouter as Router , Route} from 'react-router-dom'
+import {BrowserRouter as Router , Route , Routes} from 'react-router-dom'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
@@ -81,10 +81,30 @@ const toggleReminder=async (id)=>{
  return (
   <Router>
   <div className='container'>
-    <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
-    {showAddTask && <AddTask onAdd={addTask}/>}
-    {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks To Show' }
-    <Footer />
+    <Header 
+      onAdd={() => setShowAddTask(!showAddTask)} 
+      showAdd={showAddTask} 
+    /> 
+  <Routes>
+    <Route 
+    path='/' element={
+      <>
+        {showAddTask && <AddTask onAdd={addTask}/>}
+        {tasks.length > 0 ?(
+        <Tasks 
+        tasks={tasks} 
+        onDelete={deleteTask} 
+        onToggle={toggleReminder}
+      /> 
+    ): (
+      'No Tasks To Show' 
+    )}
+      </>
+    } 
+    />
+    <Route path='/about' element={<About />} />
+  </Routes>
+  <Footer />
   </div>
   </Router>
  )
