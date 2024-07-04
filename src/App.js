e33b407144fa6e114f04210540b4ad3658d11ca5
ChakrabortyDiya <crs.diya@gmail.com>
2024-07-04 14:12:00 +0530

    About added

--- src/App.js
+++ src/App.js
@@ -79,14 +79,14 @@ const toggleReminder=async (id)=>{
 }
 
  return (
-  <route>
+  <Router>
   <div className='container'>
     <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
     {showAddTask && <AddTask onAdd={addTask}/>}
     {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks To Show' }
     <Footer />
   </div>
-  </route>
+  </Router>
  )
 }
 
