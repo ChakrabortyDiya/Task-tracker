f95a6dd28c43bcc89bfcca8c76679effd9eb49f5
ChakrabortyDiya <crs.diya@gmail.com>
2024-07-04 10:35:40 +0530

    feature to add task is added

--- src/App.js
+++ src/App.js
@@ -28,7 +28,9 @@ const App=() =>{
 
 //add task
 const addTask = (task) => {
-  console.log(task)
+  const id= Math.floor(Math.random() * 10000) + 1
+  const newTask = {id,...task}
+  setTasks([...tasks,newTask])
 }
 
 //delete task
