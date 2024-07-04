474923bcb86802502b44fcece864bfcb7a278271
ChakrabortyDiya <crs.diya@gmail.com>
2024-07-04 13:23:50 +0530

    Add updated

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ const addTask =async  (task) => {
   })
 
   const data= res.json()
-  
+  setTasks([...tasks,data])
 
   //const id= Math.floor(Math.random() * 10000) + 1
   //const newTask = {id,...task}
