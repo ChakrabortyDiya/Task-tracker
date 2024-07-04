a57b1e77fef965c8cc17f5dd166ed502daf51e02
ChakrabortyDiya <crs.diya@gmail.com>
2024-07-04 14:11:05 +0530

    About updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { useState, useEffect } from 'react'
+import {BrowserRouter as Router , Route} from 'react-router-dom'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
 import Footer from './components/Footer'
+import About from './components/About'
 
 const App=() =>{
   const [showAddTask , setShowAddTask]= useState(false)
@@ -77,12 +79,14 @@ const toggleReminder=async (id)=>{
 }
 
  return (
+  <route>
   <div className='container'>
     <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
     {showAddTask && <AddTask onAdd={addTask}/>}
     {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks To Show' }
     <Footer />
   </div>
+  </route>
  )
 }
 
