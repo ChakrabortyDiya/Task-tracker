287354ae9ae3d25f93ab4cbf77bcc6784d47f621
ChakrabortyDiya <crs.diya@gmail.com>
2024-07-04 12:38:44 +0530

    data fetch updated

--- src/App.js
+++ src/App.js
@@ -8,15 +8,18 @@ const App=() =>{
   const [tasks,setTasks]=useState([])
 
   useEffect(() => {
-    
-    fetchTasks()
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+    getTasks()
   },[])
 
 //fetch tasks
 const fetchTasks = async() => {
   const res = await fetch('http://localhost:5000/tasks')
   const data = await res.json()
-
+  
   return data
 }
 
